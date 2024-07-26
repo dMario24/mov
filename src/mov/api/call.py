8bf2fcc07c0a56e53978349d5973a8b75feb74e1
dMario24 <data.mario24@gmail.com>
2024-07-26 13:22:07 +0900

    200 ok

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -1,6 +1,18 @@
+import requests
+
 def req(dt="20120101"):
+    #url = gen_url('20240720')
+    url = gen_url(dt)
+    r = requests.get(url)
+    code = r.status_code
+    data = r.json()
+    print(data)
+    return code, data
+
+
+def gen_url(dt="20120101"):
     base_url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json"
     key = "2820df87eeb6a918654941ca8a918b2b"
     url = f"{base_url}?key={key}&targetDt={dt}"
-    print(url)
 
+    return url
