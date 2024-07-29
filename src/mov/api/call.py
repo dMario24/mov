3ea2bfb00998e059d87cb5f0b71bde1a0ff6f49a
dMario24 <data.mario24@gmail.com>
2024-07-29 11:35:25 +0900

    save2df

--- src/mov/api/call.py
+++ src/mov/api/call.py
@@ -2,6 +2,16 @@ import requests
 import os
 import pandas as pd
 
+def save2df():
+    df = list2df()
+    # df 에 load_dt 컬럼 추가 (조회 일자 YYYYMMDD 형식 으로)
+    # 아래 파일 저장시 load_dt 기분으로 파티셔닝
+    df['load_dt'] = '20120101'
+    print(df.head(5))
+    df.to_parquet('~/tmp/test_parquet', partition_cols=['load_dt'])
+    return df
+
+
 def list2df():
     l = req2list()
     df = pd.DataFrame(l)
